1b39952781353e7b5a2f84a0fac7072983e9c5ef
chrisdugne <chris.dugne@uralys.com>
2021-07-12 16:36:28 +0200

    introducing playground.devHUD

--- extension/screens/playground.gd
+++ extension/screens/playground.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # ------------------------------------------------------------------------------
 
-func onOpen(params):
-  print('[🤖 Godox] [Playground] runs')
-  if params != null:
-    assert(params)
+func onOpen(_params):
+  print('[🤖 Godox] [Playground] onOpen')
+
+  move_child($devHUD, get_child_count() -1)
