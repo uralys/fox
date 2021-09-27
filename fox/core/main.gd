3f8b6224b7b8f94cff4486f6fecb815475e47d35
chrisdugne <chris.dugne@uralys.com>
2021-09-27 22:35:26 +0200

    started libs, warning when running from fox

--- fox/core/main.gd
+++ fox/core/main.gd
@@ -4,6 +4,7 @@ func _ready():
   print('-------------------------------')
   print('[🦊 1.0]')
   print('-------------------------------')
+  randomize() # https://docs.godotengine.org/en/latest/tutorials/math/random_number_generation.html#the-randomize-method
   startScreen()
 
 func startScreen():
