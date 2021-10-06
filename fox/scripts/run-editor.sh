d3445d3e118f1e56055d6ff5317aedacb5c40e42
chrisdugne <chris.dugne@uralys.com>
2021-10-06 23:55:34 +0200

    updated run scripts

---
+++ fox/scripts/run-editor.sh
@@ -0,0 +1,4 @@
+echo '----------------------------'
+echo '🦊 starting Godot Editor'
+echo '----------------------------'
+/Applications/Apps/Godot.app/Contents/MacOS/Godot -e --windowed --resolution 2980x2220 --position 50,170
