7a46d0eca9cfadae461484c31c177a02682d5a32
chrisdugne <chris.dugne@uralys.com>
2021-10-15 21:59:39 +0200

    introduced camera tweening between 2 positions

--- fox/components/draggable-camera.gd
+++ fox/components/draggable-camera.gd
@@ -2,9 +2,11 @@ extends Camera2D
 
 # ------------------------------------------------------------------------------
 
+var tween
 var mouse_start_pos
 var screen_start_position
 
+var tweening = false
 var dragging = false
 var moving = false
 
@@ -14,51 +16,73 @@ export(float) var pan_smooth := -5
 
 # ------------------------------------------------------------------------------
 
-var _cur_vel := Vector2(0,0)
+var draggingVelocity := Vector2(0,0)
 var _last_cam_pos := Vector2(0,0)
 
 # ------------------------------------------------------------------------------
 
+func _ready():
+  tween = Tween.new()
+  add_child(tween)
+
+# ------------------------------------------------------------------------------
+
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			mouse_start_pos = event.position
-			screen_start_position = position
-			dragging = true
-		else:
-			dragging = false
-	elif event is InputEventMouseMotion and dragging:
-		position = zoom * (mouse_start_pos - event.position) + screen_start_position
+  if event is InputEventMouseButton:
+    if event.is_pressed():
+      mouse_start_pos = event.position
+      screen_start_position = position
+      dragging = true
+    else:
+      dragging = false
+  elif event is InputEventMouseMotion and dragging:
+    position = zoom * (mouse_start_pos - event.position) + screen_start_position
 
 # ------------------------------------------------------------------------------
 
 func _process(delta):
-	if delta <= 0:
-		return
+  if delta <= 0:
+    return
 
-	if dragging:
-		update_vel(delta)
-	else:
-		smooth(delta)
+  if dragging:
+    update_vel(delta)
+  else:
+    smooth(delta)
 
-	var diff= _last_cam_pos - self.position
-	moving = diff.length() > 1
+  var diff= _last_cam_pos - self.position
+  moving = diff.length() > 1
 
-	_last_cam_pos = self.position
+  _last_cam_pos = self.position
 
 # ------------------------------------------------------------------------------
 
 func update_vel(delta : float):
-	var move = _last_cam_pos - self.position
-	var move_speed:Vector2 = move / delta
-	_cur_vel = (_cur_vel + move_speed) / 2.0
-	_cur_vel.x = clamp(_cur_vel.x, -10000, 10000)
-	_cur_vel.y = clamp(_cur_vel.y, -10000, 10000)
+  var move = _last_cam_pos - self.position
+  var move_speed:Vector2 = move / delta
+  draggingVelocity = (draggingVelocity + move_speed) / 2.0
+  draggingVelocity.x = clamp(draggingVelocity.x, -10000, 10000)
+  draggingVelocity.y = clamp(draggingVelocity.y, -10000, 10000)
+
+# ------------------------------------------------------------------------------
+
+func toPosition(from: Vector2, to : Vector2):
+  tween.interpolate_property(
+    self,
+    'position',
+    from, to,
+    1,
+    Tween.TRANS_QUAD, Tween.EASE_OUT
+  )
+
+  tween.start()
 
 # ------------------------------------------------------------------------------
 
 func smooth(delta : float):
-	var l = _cur_vel.length()
-	var move_frame = 10 * exp(pan_smooth * ((log(l/10) / pan_smooth)+delta))
-	_cur_vel = _cur_vel.normalized() * move_frame
-	self.position -= _cur_vel * delta
+  if(tweening):
+    return
+
+  var l = draggingVelocity.length()
+  var move_frame = 10 * exp(pan_smooth * ((log(l/10) / pan_smooth)+delta))
+  draggingVelocity = draggingVelocity.normalized() * move_frame
+  self.position -= draggingVelocity * delta
