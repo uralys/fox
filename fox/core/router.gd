d89f7f7f7e078ab146b5180ce37ce0360c47ab9e
chrisdugne <chris.dugne@uralys.com>
2021-10-19 16:23:14 +0200

    improved Move and router.curtain

--- fox/core/router.gd
+++ fox/core/router.gd
@@ -4,7 +4,17 @@ extends Node
 
 # ------------------------------------------------------------------------------
 
+var CURTAIN_DURATION = 0.7 #s
 var currentScene = null
+
+var leftCurtain
+var rightCurtain
+
+# ------------------------------------------------------------------------------
+
+func _ready():
+  leftCurtain = $'/root/app/curtain/left'
+  rightCurtain = $'/root/app/curtain/right'
 
 # ------------------------------------------------------------------------------
 
@@ -17,18 +27,25 @@ func prepare():
 # ------------------------------------------------------------------------------
 
 func _openScene(scene):
-  if currentScene != null:
+  if(currentScene):
+    print('[🦊 Router]> leaving current' + str(currentScene.name))
+    closeCurtain()
+
+    var _timer = Wait.start(leftCurtain, CURTAIN_DURATION)
+    yield(_timer, 'timeout')
+
     currentScene.queue_free()
 
-  # Instance the new scene.
   currentScene = scene.instance()
   print('[🦊 Router]> ' + str(currentScene.name))
-
-
-  var leftCurtain = $'/root/app/curtain/left'
-  var rightCurtain = $'/root/app/curtain/right'
-
-  Move.addValue(leftCurtain, 'anchor_right', +0.66)
-  Move.addValue(rightCurtain, 'anchor_left', -0.66)
-
   $'/root/app/scene'.add_child(currentScene)
+
+# ------------------------------------------------------------------------------
+
+func closeCurtain():
+  Move.setValue(leftCurtain, 'anchor_right', 0.66, CURTAIN_DURATION)
+  Move.setValue(rightCurtain, 'anchor_left', 0.33, CURTAIN_DURATION)
+
+func openCurtain():
+  Move.setValue(leftCurtain, 'anchor_right', 0, CURTAIN_DURATION, 0, Tween.EASE_IN)
+  Move.setValue(rightCurtain, 'anchor_left', 1, CURTAIN_DURATION, 0, Tween.EASE_IN)
--- fox/libs/animations/move.gd
+++ fox/libs/animations/move.gd
@@ -8,24 +8,28 @@ class_name Move
 
 # ------------------------------------------------------------------------------
 
-static func addValue(object, property, valueToAdd, duration = 0.75, delay = 0):
+static func addValue(object, property, valueToAdd, duration = 0.75, delay = 0, _ease = null):
   var fromValue = object[property]
   var toValue = fromValue + valueToAdd
-  _animate(object, property, fromValue, toValue, duration, delay)
+  _animate(object, property, fromValue, toValue, duration, delay, _ease)
 
 # ------------------------------------------------------------------------------
 
-static func setValue(object, property, valueToSet, duration = 0.75, delay = 0):
+static func setValue(object, property, valueToSet, duration = 0.75, delay = 0, _ease = null):
   var fromValue = object[property]
   var toValue = valueToSet
-  _animate(object, property, fromValue, toValue, duration, delay)
+  _animate(object, property, fromValue, toValue, duration, delay, _ease)
 
 # ------------------------------------------------------------------------------
 
-static func _animate(object, property, fromValue, toValue, duration = 0.75, delay = 0):
+static func _animate(object, property, fromValue, toValue, duration = 0.75, delay = 0, _ease = null):
   if(delay > 0):
     var _timer = Wait.start(object, delay)
     yield(_timer, 'timeout')
+
+  var easing = _ease
+  if(easing == null):
+    easing = Tween.EASE_OUT
 
   var tween = Tween.new()
   object.add_child(tween)
@@ -35,7 +39,7 @@ static func _animate(object, property, fromValue, toValue, duration = 0.75, dela
     property,
     fromValue, toValue,
     duration,
-    Tween.TRANS_QUAD, Tween.EASE_OUT
+    Tween.TRANS_QUAD, easing
   )
 
   tween.start()
