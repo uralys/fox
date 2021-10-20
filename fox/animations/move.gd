c7c5df20667a484975a724fa4ed0d551a11c0a29
chrisdugne <chris.dugne@uralys.com>
2021-10-20 18:14:28 +0200

    added Move.swing

--- fox/animations/move.gd
+++ fox/animations/move.gd
@@ -21,6 +21,26 @@ static func to(object, property, toValue, duration = 0.75, delay = 0, _ease = nu
 
 # ------------------------------------------------------------------------------
 
+static func swing(object, property, toValue, duration = 0.75, delay = 0, _ease = null):
+  var easing = _ease
+  if(easing == null):
+    easing = Tween.EASE_IN_OUT
+
+  var fromValue = object[property]
+  _animate(object, property, fromValue, toValue, duration, delay, easing)
+
+  var _timer = Wait.start(object, duration + delay)
+  yield(_timer, 'timeout')
+
+  _animate(object, property, toValue, fromValue, duration, 0, easing)
+
+  var _timerBack = Wait.start(object, duration)
+  yield(_timerBack, 'timeout')
+
+  swing(object, property, toValue, duration, 0, easing)
+
+# ------------------------------------------------------------------------------
+
 static func _animate(object, property, fromValue, toValue, duration = 0.75, delay = 0, _ease = null):
   if(delay > 0):
     var _timer = Wait.start(object, delay)
