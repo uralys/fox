bae8b208cfd4ef3d846f96a85ab6df653385234a
chrisdugne <chris.dugne@uralys.com>
2021-11-14 22:01:45 +0100

    added time control between press on button behaviour

--- fox/behaviours/button.gd
+++ fox/behaviours/button.gd
@@ -4,11 +4,20 @@ signal onPress
 signal onRelease
 
 export(bool) var onlyOnce = true
+export(int) var MIN_MS_BETWEEN_PRESS = 700
 
 var nbPressed = 0
+var lastPress = OS.get_ticks_msec()
 
 func _gui_input(event):
   if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+    var now = OS.get_ticks_msec()
+    var elapsedTime = now - lastPress
+
+    if(int(elapsedTime) < MIN_MS_BETWEEN_PRESS):
+      return
+
+    lastPress = now
     nbPressed += 1
 
     if(onlyOnce and nbPressed > 1):
