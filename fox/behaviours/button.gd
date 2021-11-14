c74ccf11dc47f2d55cbb84b776cecd4d90addc13
chrisdugne <chris.dugne@uralys.com>
2021-11-14 19:12:41 +0100

    button behaviour now defauls to onlyOnce

--- fox/behaviours/button.gd
+++ fox/behaviours/button.gd
@@ -3,8 +3,17 @@ extends TextureRect
 signal onPress
 signal onRelease
 
+export(bool) var onlyOnce = true
+
+var nbPressed = 0
+
 func _gui_input(event):
   if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
+    nbPressed += 1
+
+    if(onlyOnce and nbPressed > 1):
+      return
+
     emit_signal("onPress")
 
   if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and not event.pressed:
