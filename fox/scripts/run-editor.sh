b14df22c9a747f85c9aef63d87441e1160015106
chrisdugne <chris.dugne@uralys.com>
2021-11-16 14:53:06 +0100

    put back editor resolution

--- fox/scripts/run-editor.sh
+++ fox/scripts/run-editor.sh
@@ -1,5 +1,5 @@
 echo '----------------------------'
 echo '🦊 starting Godot Editor'
 echo '----------------------------'
-# /Applications/Apps/Godot.app/Contents/MacOS/Godot -e --windowed --resolution 2980x2220 --position 0,0
-/Applications/Apps/Godot.app/Contents/MacOS/Godot -e --windowed --resolution 1980x1220 --position 0,0
+/Applications/Apps/Godot.app/Contents/MacOS/Godot -e --windowed --resolution 2980x2220 --position 0,0
+# /Applications/Apps/Godot.app/Contents/MacOS/Godot -e --windowed --resolution 1980x1220 --position 0,0
