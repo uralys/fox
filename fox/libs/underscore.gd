4eec6e4de11898909fede38efe32b7711c081f3a
chrisdugne <chris.dugne@uralys.com>
2022-03-11 11:35:00 +0100

    handling TYPE_INT indexes with __.Get

--- fox/libs/underscore.gd
+++ fox/libs/underscore.gd
@@ -15,8 +15,23 @@ static func Get(path, obj):
   if(not path):
     return obj
 
+  if(typeof(path) == TYPE_INT):
+    if(obj.has(path)):
+      return obj[path]
+    else:
+      prints('🔴 __.Get(path, obj): index not found:', path);
+      return null
+
+  if(typeof(path) != TYPE_STRING):
+    prints('🔴 __.Get(path, obj): path must be either a TYPE_INT or a TYPE_STRING');
+    return null
+
+  # ---
+
   var res = obj
   var fields = Array(path.split('.'))
+
+  # ---
 
   while(fields.size() > 0):
     var field = fields.pop_front()
