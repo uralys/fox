27fadfde91d246272aae9d83f7b9080c64a48074
Chris <chris.dugne@uralys.com>
2023-10-18 19:10:41 +0200

    allowing longPress on button and draggable

--- fox/behaviours/button.gd
+++ fox/behaviours/button.gd
@@ -1,16 +1,32 @@
 extends Node
 
-signal onPress
+signal press
+signal longPress
 
 @export var onlyOnce: bool = true
 @export var sound: bool = true
 @export var MIN_MS_BETWEEN_PRESS: int = 700
 
+var pressing = false
 var nbPressed = 0
 var lastPress = Time.get_ticks_msec()
 
+# ------------------------------------------------------------------------------
+
+func _physics_process(_delta):
+  if pressing:
+    var now = Time.get_ticks_msec()
+    var elapsedTime = now - lastPress
+
+    if(elapsedTime > 750):
+      emit_signal('longPress')
+      pressing = false
+
+# ------------------------------------------------------------------------------
+
 func _gui_input(event):
   if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+    pressing = true
     var now = Time.get_ticks_msec()
     var elapsedTime = now - lastPress
 
@@ -23,7 +39,14 @@ func _gui_input(event):
     if(onlyOnce and nbPressed > 1):
       return
 
-    emit_signal('onPress')
-
     # if(sound):
     #   Sound.play(Sound.BUTTON_PRESS)
+
+  if pressing \
+  and event is InputEventMouseButton \
+  and event.button_index == MOUSE_BUTTON_LEFT \
+  and not event.pressed:
+    # mouse up
+    pressing = false
+
+    emit_signal('press')
--- fox/behaviours/draggable.gd
+++ fox/behaviours/draggable.gd
@@ -2,38 +2,81 @@ extends Control
 
 # ------------------------------------------------------------------------------
 
-var parent = false
+var draggable
 var params = {}
 var mouseStartPosition
 var screenStartPosition
 
+@export var afterLongPress: bool = false
+
+var dragging = false
+var pressing = false
+var lastPress = Time.get_ticks_msec()
+
 # ------------------------------------------------------------------------------
 
 signal dragged
+signal startedDragging
 
 # ------------------------------------------------------------------------------
 
 func _ready():
   connect('gui_input' , _gui_input)
   params.id = generateUID.withPrefix('draggable')
-  parent = get_parent();
+
+# ------------------------------------------------------------------------------
+
+func _physics_process(_delta):
+  if(!afterLongPress):
+    return
+
+  if pressing:
+    var now = Time.get_ticks_msec()
+    var elapsedTime = now - lastPress
+
+    if(elapsedTime > 500):
+      var mousePosition = get_global_mouse_position()
+      pressing = false
+
+      if(abs(mouseStartPosition - mousePosition).length() > 1):
+        return
+
+      emit_signal('startedDragging')
+      startDragging()
+
+# ------------------------------------------------------------------------------
+
+func startDragging():
+  dragging = true
+  Display.DRAGGING_OBJECT = params.id
+  screenStartPosition = draggable.position
 
 # ------------------------------------------------------------------------------
 
 func _gui_input(event):
   if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-    Display.DRAGGING_OBJECT = params.id
+    lastPress = Time.get_ticks_msec()
     mouseStartPosition = get_global_mouse_position()
-    screenStartPosition = parent.position
+
+    if(not afterLongPress):
+      startDragging()
+    else:
+      pressing = true
+
     return
 
-  if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and !event.pressed:
+  if event is InputEventMouseButton \
+  and event.button_index == MOUSE_BUTTON_LEFT \
+  and !event.pressed:
+    if(dragging):
+      emit_signal('dragged', draggable.position)
+
     Display.DRAGGING_OBJECT = null
+    dragging = false
+    pressing = false
     return
 
-  if event is InputEventMouseMotion and Display.DRAGGING_OBJECT == params.id:
-    parent.position = (get_global_mouse_position() - mouseStartPosition) + screenStartPosition
-
-    Wait.withTimer(0.3, self, func():
-      emit_signal('dragged', parent.position)
-    )
+  if dragging \
+  and event is InputEventMouseMotion \
+  and Display.DRAGGING_OBJECT == params.id:
+    draggable.position = (get_global_mouse_position() - mouseStartPosition) + screenStartPosition
