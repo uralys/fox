17358eeadf975d0f03563f348a52a5991938e7e0
Chris <chris.dugne@uralys.com>
2023-12-22 14:22:29 +0100

    improved screenshots generation: using folders

--- cli/generate-screenshots.js
+++ cli/generate-screenshots.js
@@ -14,14 +14,28 @@ import shell from 'shelljs';
 
 const generateScreenshots = (config) => {
   const {orientation, input, output, sizes} = config;
-  console.log(`⚙️  resizing ${chalk.blue.bold('screenshots')}...`);
+  const projectPath = path.resolve(process.cwd(), './');
+
+  console.log(`\n⚙️  veryfing folders ...`);
+
+  sizes.forEach(({name}) => {
+    const sizeFolder = `${projectPath}/${output}/${name}`;
+
+    if (!fs.existsSync(sizeFolder)) {
+      shell.mkdir('-p', sizeFolder);
+      console.log(`✅ created ${sizeFolder}`);
+    }
+  })
+
+
+  console.log(`\n⚙️  resizing ${chalk.blue.bold('screenshots')}...`);
   console.log(`⚙️  orientation: ${chalk.blue.bold(orientation)}`);
 
   const files = fs.readdirSync(input);
 
   files.forEach((fileName) => {
     const extension = path.extname(fileName);
-    if (!['.png', '.jpg', '.jpeg'].includes(extension)) {
+    if (!['.png', '.jpg', '.jpeg', '.webp'].includes(extension)) {
       return;
     }
 
@@ -29,10 +43,12 @@ const generateScreenshots = (config) => {
       const resolution = orientation === 'landscape' ? size.resolution : size.resolution.split('x').reverse().join('x');
 
       const outputFileName = `${fileName.split(extension)[0]}-${size.name}-${resolution}${extension}`;
-      console.log(`\n > ${chalk.magenta.italic(fileName)} | ${chalk.magenta.italic(size.name)} --> ${output}/${outputFileName}`);
+      const outputPath = `${output}/${size.name}/${outputFileName}`;
+
+      console.log(`\n > ${chalk.magenta.italic(fileName)} | ${chalk.magenta.italic(size.name)} --> ${outputPath}`);
 
       shell.exec(
-        `convert ${input}/${fileName} -resize ${resolution}^ -gravity center -extent ${resolution} "${output}/${outputFileName}"`
+        `convert ${input}/${fileName} -resize ${resolution}^ -gravity center -extent ${resolution} "${outputPath}"`
       );
 
       console.log(`Resized images to ${resolution} ${chalk.green('successfully')}.`);
