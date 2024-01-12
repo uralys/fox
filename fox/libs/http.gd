67e4b055448dcdb19a7989a8c2af87d0806ef1af
Chris <chris.dugne@uralys.com>
2024-01-12 23:26:52 +0100

    improved HTTP Get + Post

--- fox/libs/http.gd
+++ fox/libs/http.gd
@@ -8,22 +8,49 @@ class_name HTTP
 
 # ------------------------------------------------------------------------------
 
-static func Get(caller, options):
+static var API_URL = ProjectSettings.get_setting('custom/api-url')
+static var API_KEY = ProjectSettings.get_setting('custom/api-key')
+
+# ------------------------------------------------------------------------------
+
+static func _createHTTPRequest(caller, options):
   var subURL = __.GetOr('', 'url' ,options)
   var onComplete = __.Get('onComplete', options)
 
-  var apiUrl = ProjectSettings.get_setting('api/url')
-  var url = apiUrl + subURL
+  var url = API_URL + subURL
 
   var http = HTTPRequest.new()
   caller.add_child(http)
 
   if(onComplete):
-    http.connect('request_completed', Callable(caller,onComplete))
+    http.request_completed.connect(onComplete)
 
-  var API_KEY = ProjectSettings.get_setting('api/key')
+  return {http=http, url=url}
+
+# ------------------------------------------------------------------------------
+
+static func Get(caller, options):
+  var httpRequest = _createHTTPRequest(caller, options)
+  var http = httpRequest.http
+  var url = httpRequest.url
 
   http.request(
     url,
-    ['x-api-key:' + API_KEY]
+    ['x-api-key:' + API_KEY],
+    HTTPClient.METHOD_GET
+  )
+
+# ------------------------------------------------------------------------------
+
+static func Post(caller, options):
+  var httpRequest = _createHTTPRequest(caller, options)
+  var body = __.Get('body', options)
+  var http = httpRequest.http
+  var url = httpRequest.url
+
+  http.request(
+    url,
+    ['x-api-key:' + API_KEY],
+    HTTPClient.METHOD_POST,
+    body
   )
