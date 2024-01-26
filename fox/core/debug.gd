ce2d9241f23a2d97ef13c4870fb23eea4a1f92ac
Chris <chris.dugne@uralys.com>
2024-01-26 23:55:25 +0100

    fixed DEBUG

--- fox/core/debug.gd
+++ fox/core/debug.gd
@@ -5,8 +5,10 @@ extends Node
 # ------------------------------------------------------------------------------
 
 func setup():
+  var _options = self.options if self.options else {}
+
   if(G.ENV == G.RELEASE):
-    for option in options:
+    for option in _options:
       self[option] = false
 
     G.log('✅ debug options deactivated in production');
@@ -14,7 +16,7 @@ func setup():
 
   var debugOptionsEnabled = false
 
-  for option in options:
+  for option in _options:
     if(self[option]):
       G.log(' >', option, '[color=pink]is activated [/color]')
       debugOptionsEnabled = true
