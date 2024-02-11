213ba839265c8338453e75f86bee9109e81b82d0
Chris <chris.dugne@uralys.com>
2024-02-11 13:36:53 +0100

    adding NotificationScheduler when useNotifications=true

--- fox/core/app.gd
+++ fox/core/app.gd
@@ -5,12 +5,14 @@ extends CanvasLayer
 # ------------------------------------------------------------------------------
 
 var SplashAnimation = preload('res://fox/animations/splash-animation.tscn')
+var NotificationsScheduler = preload('res://fox/behaviours/notifications.tscn')
 
 # ------------------------------------------------------------------------------
 
 func finalizeFoxSetup():
   DEBUG.setup()
   createScreenReference()
+  prepareNotifications()
   randomize() # https://docs.godotengine.org/en/latest/tutorials/math/random_number_generation.html#the-randomize-method
 
 # ------------------------------------------------------------------------------
@@ -48,3 +50,19 @@ func recordScreenDimensions():
   G.W = screenReference.get_rect().size.x
   G.H = screenReference.get_rect().size.y
   G.SCREEN_CENTER = Vector2(G.W /2.0, G.H /2.0)
+
+# ------------------------------------------------------------------------------
+
+func prepareNotifications():
+  var useNotifications = ProjectSettings.get_setting('custom/useNotifications')
+  if(not useNotifications):
+    return
+
+  G.log('--------------');
+  G.log('Notifications are enabled');
+
+  var scheduler = NotificationsScheduler.instantiate()
+  $/root.add_child.call_deferred(scheduler)
+
+  var h = scheduler.has_post_notifications_permission()
+  G.log('has_post_notifications_permission:', h);
