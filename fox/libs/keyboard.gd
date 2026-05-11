de69c9368124d1705808e0bfa9d5e53ba15062f1
chrisdugne <chris.dugne@uralys.com>
2026-05-11 23:03:21 +0200

    added InputEventJoypadButton

--- fox/libs/keyboard.gd
+++ fox/libs/keyboard.gd
@@ -12,19 +12,33 @@ signal cancel
 # ------------------------------------------------------------------------------
 
 func _input(event):
-	if not (event is InputEventKey and event.pressed):
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_UP, KEY_W:
+				direction_up.emit()
+			KEY_DOWN, KEY_S:
+				direction_down.emit()
+			KEY_LEFT, KEY_A:
+				direction_left.emit()
+			KEY_RIGHT, KEY_D:
+				direction_right.emit()
+			KEY_SPACE, KEY_ENTER, KEY_KP_ENTER:
+				confirm.emit()
+			KEY_ESCAPE:
+				cancel.emit()
 		return
 
-	match event.keycode:
-		KEY_UP, KEY_W:
-			direction_up.emit()
-		KEY_DOWN, KEY_S:
-			direction_down.emit()
-		KEY_LEFT, KEY_A:
-			direction_left.emit()
-		KEY_RIGHT, KEY_D:
-			direction_right.emit()
-		KEY_SPACE, KEY_ENTER, KEY_KP_ENTER:
-			confirm.emit()
-		KEY_ESCAPE:
-			cancel.emit()
+	if event is InputEventJoypadButton and event.pressed:
+		match event.button_index:
+			JOY_BUTTON_DPAD_UP:
+				direction_up.emit()
+			JOY_BUTTON_DPAD_DOWN:
+				direction_down.emit()
+			JOY_BUTTON_DPAD_LEFT:
+				direction_left.emit()
+			JOY_BUTTON_DPAD_RIGHT:
+				direction_right.emit()
+			JOY_BUTTON_A:
+				confirm.emit()
+			JOY_BUTTON_B:
+				cancel.emit()
